3fb6b9c974119fc3a7fa7e3ea69a88f93569f9b7
Enerel Tsogt-Ochir <enerel.tsogtochir@gmail.com>
2018-03-19 21:30:37 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<div class="ebbu">
+
+		<a href="resume.php">
+		Resume
+		</a>
+
+		<a href="opt.php">
+		OPT
+		</a>
+		<a href="cpt.php">
+		CPT
+		</a>
+</div>
+
+<div class="picture">
+				<img src='resume_pic.jpg' alt='working on a laptop' />
+			</div>
+
+
+</body>
+</html>
